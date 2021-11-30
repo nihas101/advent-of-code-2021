7862f0efae8fd7e2f5af48faf67a9cd8ef4b4eb3
Nikita Hasert <nikitahasert@gmail.com>
2021-11-30 20:03:35 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent_of_code_2021 "1.0.0"
+  :description "Solutions to Advent of code 2020"
+  :url "https://github.com/nihas101/advent-of-code-2021"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]])
---
+++ src/advent_of_code_2021/utils.clj
@@ -0,0 +1,51 @@
+(ns advent-of-code-2021.utils
+  (:require
+   [clojure.string :as string]))
+
+;; Math
+
+(defn- abs [^long x] (max x (- x)))
+
+(defn extended-gcd
+  "https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm"
+  [^long a ^long b]
+  (loop [[o-r r] [(long a) (long b)]
+         [o-s s] [(long 1) (long 0)]
+         [o-t t] [(long 0) (long 1)]]
+    (if (zero? r)
+      {:x o-s :y o-t
+       :gcd o-r
+       :quots [t s]}
+      (let [q (quot o-r r)]
+        (recur [r (- ^long o-r (* ^long q ^long r))]
+               [s (- ^long o-s (* ^long q ^long s))]
+               [t (- ^long o-t (* ^long q ^long t))])))))
+
+;; The unusual definition of the lcm is so that it can be used within a transducer
+(defn lcm
+  ([] nil)
+  ([x] x)
+  ([a b]
+   (if (nil? a)
+     b
+     (quot (* ^long a ^long b) ^long (:gcd (extended-gcd a b))))))
+
+(defn crt
+  "https://brilliant.org/wiki/chinese-remainder-theorem/"
+  [as+nz]
+  (let [as (mapv first as+nz)
+        nz (mapv second as+nz)
+        N (reduce * nz)
+        ys (mapv (partial / N) nz)
+        zs (mapv :x (mapv extended-gcd ys nz))]
+    (mod (abs (reduce + (mapv * as ys zs))) N)))
+
+;; Strings
+
+(defn split-sections [s]
+  (string/split s #"(\r?\n){2,}"))
+
+(def line-endings #"\r?\n")
+
+(defn read-longs [s split-on]
+  (mapv #(Long/parseLong %) (string/split s split-on)))
