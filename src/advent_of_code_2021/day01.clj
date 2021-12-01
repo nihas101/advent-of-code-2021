5980af79a5965e3aae48d82e02cb06db98235e7c
Nikita Hasert <nikitahasert@gmail.com>
2021-12-01 22:06:56 +0100

    Day 01: Simplify solution

--- src/advent_of_code_2021/day01.clj
+++ src/advent_of_code_2021/day01.clj
@@ -2,26 +2,17 @@
   (:require
    [advent-of-code-2021.utils :as u]))
 
-
-(defn- inc-if-increased
-  ([] 0)
-  ([cnt] cnt)
-  ([cnt [_ change]]
-   (if (= change :increased)
-     (inc cnt)
-     cnt)))
-
 (defn day1-1
   "https://adventofcode.com/2021/day/1"
   ([] (day1-1
        (u/read-longs (slurp "resources/depth_measurements.txt")
                      u/line-endings)))
   ([depth-measurements]
-   (transduce
-    (map (fn [[bef aft]]
-           [aft (if (< bef aft) :increased :decreased)]))
-    inc-if-increased
-    (partition 2 1 depth-measurements))))
+   (count
+    (transduce
+     (filter (partial apply <))
+     conj []
+     (partition 2 1 depth-measurements)))))
 
 (defn day1-2
   "https://adventofcode.com/2021/day/1"
