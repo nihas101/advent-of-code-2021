c3a7bbe35ccf234c18277da09faf2c8318071e8e
Nikita Hasert <nikitahasert@gmail.com>
2021-12-02 20:03:08 +0100

    Day 1: Extract depth-measurements into var

--- src/advent_of_code_2021/day01.clj
+++ src/advent_of_code_2021/day01.clj
@@ -2,11 +2,13 @@
   (:require
    [advent-of-code-2021.utils :as u]))
 
+(def ^:private depth-measurements
+  (u/read-longs (slurp "resources/depth_measurements.txt")
+                u/line-endings))
+
 (defn day1-1
   "https://adventofcode.com/2021/day/1"
-  ([] (day1-1
-       (u/read-longs (slurp "resources/depth_measurements.txt")
-                     u/line-endings)))
+  ([] (day1-1 depth-measurements))
   ([depth-measurements]
    (count
     (transduce
@@ -16,9 +18,7 @@
 
 (defn day1-2
   "https://adventofcode.com/2021/day/1"
-  ([] (day1-2
-       (u/read-longs (slurp "resources/depth_measurements.txt")
-                     u/line-endings)))
+  ([] (day1-2 depth-measurements))
   ([depth-measurements]
    (day1-1
     (eduction
